fff0844994fc50018ba53cc06512df0cef529fbf
Emir Rıdvan Toraman <toramanemir41@gmail.com>
2025-06-13 00:41:35 +0300

    history button added, style designed

--- frontend/src/components/MainScreen.js
+++ frontend/src/components/MainScreen.js
@@ -36,7 +36,7 @@ function MainScreen({ username }) {
 
       bgRef.current.style.background = `linear-gradient(120deg, rgb(${r1},${g1},${b1}), rgb(${r2},${g2},${b2}))`;
 
-      step += 0.008;
+      step += 0.001;
       if (step >= 1) {
         step = 0;
         colorIndices[0] = colorIndices[1];
@@ -70,6 +70,12 @@ function MainScreen({ username }) {
             className="mainscreen-btn create"
           >
             <i className="fas fa-plus-circle"></i> Oyun Oluştur
+          </button>
+          <button
+            onClick={() => navigate('/quiz-history')}
+            className="mainscreen-btn history"
+          >
+            <i className="fas fa-history"></i> Quiz Geçmişi
           </button>
           <button
             className="logout-icon-btn"
